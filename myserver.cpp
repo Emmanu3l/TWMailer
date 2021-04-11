7306293ff0bcd5fc72e72d9f55735842c99d56ba
Emmanuel Najfar <if20b200@technikum-wien.at>
2021-04-11 21:20:47 +0200

    Work on ADD command

--- myserver.cpp
+++ myserver.cpp
@@ -7,9 +7,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+// The fstream library allows us to work with files.
+// To use the fstream library, include both the standard <iostream> AND the <fstream> header file:
+#include <iostream>
+#include <fstream>
 #define BUF 1024
 //#define PORT 6543
-#define PORT 6544
+//temporarily change port because I can't bind to the other one right now
+#define PORT 6543
 
 int main (void) {
   int create_socket, new_socket;
@@ -17,6 +22,10 @@ int main (void) {
   char buffer[BUF];
   int size;
   struct sockaddr_in address, cliaddress;
+
+  // my own variables
+  int fileCounter = 0; // name files starting from 1.txt etc.
+  //string message;
 
   // int sockfd = socket(domain, type, protocol)
   // sockfd: socket descriptor, an integer (like a file-handle)
@@ -61,10 +70,14 @@ int main (void) {
      if (new_socket > 0)
      {
         printf ("Client connected from %s:%d...\n", inet_ntoa (cliaddress.sin_addr),ntohs(cliaddress.sin_port));
+        // char * strcpy ( char * destination, const char * source );
+        // Copies the C string pointed by source into the array pointed by destination,
+        // including the terminating null character (and stopping at that point).
         strcpy(buffer,"Welcome to myserver, Please enter your command:\n");
         send(new_socket, buffer, strlen(buffer),0);
      }
      do {
+         // Receive a reply from the server
         size = recv (new_socket, buffer, BUF-1, 0);
         if( size > 0)
         {
@@ -72,17 +85,45 @@ int main (void) {
            printf ("Message received: %s\n", buffer);
            // message received --> do stuff
            // begin my code
-           //there seems to be a character add the end of the message which I have to remove
-           //A better solution [for comparing string data received from a socket in C],
+           // there seems to be a character add the end of the message which I have to remove
+           // A better solution [for comparing string data received from a socket in C],
            // which does not depend on the received data being null terminated is to use memcmp:
             if (memcmp(buffer, "ADD", strlen("ADD")) == 0) {
-                printf("ADD COMMAND RECEIVED\n");
+                printf("ADD COMMAND RECOGNIZED\n");
+                // get contents of buffer after \n
+                // add every line after that into the file, including the new line
+                // stop listening after ".\n" and save the file
+
+                // To create a file, use either the ofstream or fstream class, and specify the name of the file.
+                // To write to the file, use the insertion operator (<<).
+
+                std::string filename = std::to_string(fileCounter) + ".txt";
+                ++fileCounter;
+
+                // Create and open a text file
+                std::ofstream MyFile(filename);
+
+                // starting in the array after the command,
+                // read the contents of the array
+                // as long as the message isn't ".\n":
+                do {
+                    // Write to the file
+                    // recv again
+                    //MyFile << recv (new_socket, buffer, BUF-1, 0); //something goes wrong here, the wrong characters are put in the file
+                    size = recv (new_socket, buffer, BUF-1, 0);
+                    buffer[size] = '\0';
+                    MyFile << buffer;
+                }
+                while (memcmp(buffer, "\n.", strlen("\n.")) != 0);
+                // Close the file
+                MyFile.close();
+
             } else if (memcmp(buffer, "LIST", strlen("LIST")) == 0) {
-                printf("LIST COMMAND RECEIVED\n");
+                printf("LIST COMMAND RECOGNIZED\n");
             } else if (memcmp(buffer, "QUOTE", strlen("QUOTE")) == 0) {
-                printf("QUOTE COMMAND RECEIVED\n");
+                printf("QUOTE COMMAND RECOGNIZED\n");
             } else if (memcmp(buffer, "LOGOUT", strlen("LOGOUT")) == 0) {
-                printf("LOGOUT COMMAND RECEIVED\n");
+                printf("LOGOUT COMMAND RECOGNIZED\n");
             } else {
                 printf("COULD NOT READ COMMAND\n");
             }
