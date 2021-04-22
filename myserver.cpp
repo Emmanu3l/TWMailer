5e89278888603af63346cf9a14538a4ff4c8ca83
Emmanuel Najfar <if20b200@technikum-wien.at>
2021-04-22 13:49:32 +0200

    Implement threading further, adjust CMake to include pthread

--- myserver.cpp
+++ myserver.cpp
@@ -15,14 +15,44 @@
 #define BUF 1024
 //#define PORT 6543
 //temporarily change port because I can't bind to the other one right now
-#define PORT 6644 //maybe i need to iterate through ports at some point? need to look up where the free ones start and end
+#define PORT 6644 // maybe i need to iterate through ports at some point? need to look up where the free ones start and end
+#define BACKLOG 5 // number of connections i will accept at once, using 5 because it was specified in the server sample
 
 // TODO: concurrency
-// TODO: work on error handling e.g. if(Myfile.is_open()){...} oder if (socket creation, bind oder listen == failed == return -1) -> do something. Also wenn die funktionen -1 zurückgeben dann stimmt etwas nicht
+// TODO: work on error handling e.g. if(Myfile.is_open()){...}
+//  oder if (socket creation, bind oder listen == failed == return -1) -> do something. Also wenn die funktionen -1 zurückgeben dann stimmt etwas nicht
+//  oder buffer overflow etc
 // TODO: write code to handle possible changes/adjustments to the exercise
 // TODO: test Makefile
 // TODO: man soll den server mit einem port und verzeichnis als parameter starten
-int handleRequest(int size, int new_socket, char buffer[BUF], int fileCounter, int create_socket) { // in order to have concurrency, I should also have function that can be called for each request
+// TODO: function prototype?
+
+int create_socket, new_socket;
+socklen_t addrlen;
+char buffer[BUF];
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // i guess this is obligatory
+int size;
+struct sockaddr_in address, cliaddress;
+
+// my own variables
+int fileCounter = 1; // name files starting from 1.txt etc.
+
+// int sockfd = socket(domain, type, protocol)
+// sockfd: socket descriptor, an integer (like a file-handle)
+
+// domain: integer, communication domain e.g., AF_INET (IPv4 protocol) , AF_INET6 (IPv6 protocol)
+
+// type: communication type
+// SOCK_STREAM: TCP(reliable, connection oriented) [was in der angabe verlangt wird]
+// SOCK_DGRAM: UDP(unreliable, connectionless)
+
+
+
+//thread functions need to return a void pointer and they need to accept a pointer
+//int size, int new_socket, char buffer[BUF], int fileCounter, int create_socket
+void * handleRequest(void* pointer_create_socket) { // in order to have concurrency, I should also have function that can be called for each request
+    int create_socket = *((int*)pointer_create_socket);
+    free(pointer_create_socket); // not needed any longer
     do {
         // Receive a reply from the server
         size = recv (new_socket, buffer, BUF-1, 0);
@@ -114,8 +144,9 @@ int handleRequest(int size, int new_socket, char buffer[BUF], int fileCounter, i
 
             } else if (memcmp(buffer, "LOGOUT", strlen("LOGOUT")) == 0) {
                 printf("LOGOUT COMMAND RECOGNIZED\n");
-                close (create_socket);
-                return EXIT_SUCCESS; // or should I put a break here?
+                //close (create_socket); TODO
+                //return EXIT_SUCCESS; // or should I put a break here?
+                return NULL; //replace return values with NULL to appease the compiler, since now our function is supposed to return a pointer
             } else {
                 printf("COULD NOT READ COMMAND\n");
             }
@@ -129,32 +160,14 @@ int handleRequest(int size, int new_socket, char buffer[BUF], int fileCounter, i
         else
         {
             perror("recv error");
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE; TODO
+            return NULL; //replace return values with NULL to appease the compiler, since now our function is supposed to return a pointer
         }
     } while (strncmp (buffer, "quit", 4)  != 0);
     close (new_socket);
 }
 
 int main (void) {
-  int create_socket, new_socket;
-  socklen_t addrlen;
-  char buffer[BUF];
-  pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // i guess this is obligatory
-  int size;
-  struct sockaddr_in address, cliaddress;
-
-  // my own variables
-  int fileCounter = 1; // name files starting from 1.txt etc.
-
-  // int sockfd = socket(domain, type, protocol)
-  // sockfd: socket descriptor, an integer (like a file-handle)
-
-  // domain: integer, communication domain e.g., AF_INET (IPv4 protocol) , AF_INET6 (IPv6 protocol)
-
-  // type: communication type
-  // SOCK_STREAM: TCP(reliable, connection oriented) [was in der angabe verlangt wird]
-  // SOCK_DGRAM: UDP(unreliable, connectionless)
-
   // protocol: Protocol value for Internet Protocol(IP), which is 0.
   // This is the same number which appears on protocol field in the IP header of a packet.(man protocols for more details)
   create_socket = socket (AF_INET, SOCK_STREAM, 0);
@@ -175,7 +188,7 @@ int main (void) {
   // It puts the server socket in a passive mode, where it waits for the client to approach the server to make a connection.
   // The backlog, defines the maximum length to which the queue of pending connections for sockfd may grow.
   // If a connection request arrives when the queue is full, the client may receive an error with an indication of ECONNREFUSED.
-  listen (create_socket, 5);
+  listen (create_socket, BACKLOG); // so there was already a backlog of 5 specified
   
   addrlen = sizeof (struct sockaddr_in);
 
@@ -200,8 +213,13 @@ int main (void) {
         //whenever a connection has been established, i should call the handleRequest function
      }
 
-     return handleRequest(size, new_socket, buffer, fileCounter, create_socket);
-
+     //return handleRequest(size, new_socket, buffer, fileCounter, create_socket);
+     pthread_t myThread; // pthread_t variable that keeps track of my thread
+     //int *pointer_client = (int*)malloc(sizeof(int)); // less elegant, old fashioned c-esque way to do it
+     int *pointer_create_socket = new int[sizeof(int)]; // the pointer shouldn't be interfered with by any other thread, therefore i allocate some space on the heap for an int
+     *pointer_create_socket = create_socket; // store the value of the client socket
+     pthread_create(&myThread, NULL, handleRequest, pointer_create_socket); // pass in thread address, thread function (handleRequest), and a thread argument that needs to be a pointer
+     //the function passed as the thread callback must be a (void*)(*)(void*) type function
   }
   close (create_socket);
   return EXIT_SUCCESS;
